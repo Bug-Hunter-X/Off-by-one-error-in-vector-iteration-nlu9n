d3f613caedcfdad77f17736c411ec39cffca5a3e
BugHunter-X <askzenai@gmail.com>
2025-01-03 11:10:06 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,7 @@
+std::vector<int> myVector = {1, 2, 3, 4, 5};
+
+for (int i = 0; i < myVector.size(); ++i) { //Corrected loop condition
+  std::cout << myVector[i] << " ";
+}
+
+std::cout << std::endl;
